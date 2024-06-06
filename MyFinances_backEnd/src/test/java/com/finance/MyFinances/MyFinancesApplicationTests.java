087b6e7420015c36ece8087923a592384e02dca0
imedeiros <ismaeltoledodemedeiros@gmail.com>
2024-06-06 15:51:49 -0300

    Adicionando o back-end da aplicação

---
+++ MyFinances_backEnd/src/test/java/com/finance/MyFinances/MyFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.MyFinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
